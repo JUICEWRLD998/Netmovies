54d62ede4a66f7e95210c57c91ce0f359f6f7ca5
Fadhlullah Mustapha <fadhmusty@gmail.com>
2026-01-10 20:00:26 +0100

    Native wind removed

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
+      <Text style={styles.title}>First Mobile App</Text>
       
     </View>
   );
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
   },
   title: {
-    fontSize: 48,
+    fontSize: 32,
     fontWeight: "bold",
     color: "#E50914",
   },
