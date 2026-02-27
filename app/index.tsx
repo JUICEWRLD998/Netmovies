74ba75c9ecf69ac675e8c328f2dfb8096680b563
Fadhlullah Mustapha <fadhmusty@gmail.com>
2026-02-27 11:16:25 +0100

    setup complete

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-black">
-      <Text className="text-3xl font-bold text-[#E50914]">
+      <Text className="text-5xl font-bold text-[#c4040e]">
         First Mobile App
       </Text>
     </View>
